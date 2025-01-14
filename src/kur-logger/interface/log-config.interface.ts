9039fc63992771ea89ed8a72cc790e2bbd211f08
Avnish Awasthi <avnishawasthi@tmrnd.com.my>
2025-01-14 23:43:57 +0800

    add DATE_FORMAT

--- src/kur-logger/interface/log-config.interface.ts
+++ src/kur-logger/interface/log-config.interface.ts
@@ -1,3 +1,9 @@
+export enum DATE_FORMAT {
+  YYYY=1, // Use 1 instead of zero because of checking, when 0 being ignored
+  YYYYMM,
+  YYYYMMDD,
+}
+
 export interface LogConfig {
   elasticsearch?: {
     nodes: string | string[];
